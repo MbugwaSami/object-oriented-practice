8b50744f56f7a969aaf739d12648be9e6b3d1b30
Sammy Njau <samimbugwa@gmail.com>
2018-10-06 02:23:30 -0700

    added class Questions

--- models.py
+++ models.py
@@ -6,5 +6,16 @@ class Users(object):
         self.email=email
         self.password=password
 
+class Questions(object):
+
+    def __init__(self,quiz_id,title,description,date_created,date_modified):
+        self.quiz_id=quiz_id
+        self.title=title
+        self.description=description
+        self.date_created=date_created
+        self.date_modified=date_modified
+
+
 if __name__ == '__main__':
     sammy=Users("samimbugwa@gmail.com","123ert")
+    
