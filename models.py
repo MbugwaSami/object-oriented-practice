99f5e678baf2f243765dd553c11c0232cce874ac
Sammy Njau <samimbugwa@gmail.com>
2018-10-06 02:30:00 -0700

    added class Answers

--- models.py
+++ models.py
@@ -8,12 +8,24 @@ class Users(object):
 
 class Questions(object):
 
-    def __init__(self,quiz_id,title,description,date_created,date_modified):
+    def __init__(self,quiz_id,title,description,date_created,date_modified,owner):
         self.quiz_id=quiz_id
         self.title=title
         self.description=description
         self.date_created=date_created
         self.date_modified=date_modified
+        self.owner=owner
+
+class Answers(object):
+
+    def __init__(self,ans_id,description,date_answered,date_modified,contributor):
+        self.ans_id=ans_id
+        self.description=description
+        self.date_answered=date_answered
+        self.date_modified=date_modified
+        self.contributor=contributor
+
+
 
 
 if __name__ == '__main__':
