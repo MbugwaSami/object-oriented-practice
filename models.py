4e25d369924e1327ca6baf451689dfb302914028
Sammy Njau <samimbugwa@gmail.com>
2018-10-06 02:15:55 -0700

    added class users

---
+++ models.py
@@ -0,0 +1,10 @@
+userDetails={}
+userList=[]
+class Users(object):
+
+    def __init__(self,email,password):
+        self.email=email
+        self.password=password
+
+if __name__ == '__main__':
+    sammy=Users("samimbugwa@gmail.com","123ert")
