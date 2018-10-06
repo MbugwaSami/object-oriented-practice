37883433ce3b94bc6ca5e9105b511ea6c45e7417
Sammy Njau <samimbugwa@gmail.com>
2018-10-06 03:58:54 -0700

    added the create_account function

--- models.py
+++ models.py
@@ -6,6 +6,11 @@ class Users(object):
         self.email=email
         self.password=password
 
+    def create_account(self):
+        user=Users(self.email,self.password)
+        userList.append(user)
+        print(userList)
+
 class Questions(object):
 
     def __init__(self,quiz_id,title,description,date_created,date_modified,owner):
@@ -15,6 +20,9 @@ class Questions(object):
         self.date_created=date_created
         self.date_modified=date_modified
         self.owner=owner
+
+
+
 
 class Answers(object):
 
@@ -33,4 +41,7 @@ class Admin(Users):
 
 if __name__ == '__main__':
     sammy=Users("samimbugwa@gmail.com","123ert")
+    sammy5=Users("samimbugwa@gmail.com","123ert")
     sammy2=Admin("samimbugwa@gmail.com","123ert","admin")
+    sammy.create_account()
+    sammy5.create_account()
